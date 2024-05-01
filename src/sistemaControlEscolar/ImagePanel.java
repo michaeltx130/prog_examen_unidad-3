9b3115d9df4016b8890242404be44ace21bca34d
Michelle <114263903+michaeltx130@users.noreply.github.com>
2024-05-01 09:51:59 -0700

    Exámen Unidad 3

---
+++ src/sistemaControlEscolar/ImagePanel.java
@@ -0,0 +1,30 @@
+package sistemaControlEscolar;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+
+    private BufferedImage image;
+
+    public ImagePanel() {
+       try {                
+          image = ImageIO.read(new File("C:\\Users\\inghe\\eclipse-workspace\\EXAMEN_UNIDAD3_Prog\\src\\examen\\blue.png"));
+       } catch (IOException ex) {
+            // handle exception...
+       }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);          
+    }
+
+}
